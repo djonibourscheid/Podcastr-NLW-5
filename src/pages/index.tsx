f0579c289cecfaa3735bb3f0a19c335bb6992347
Djoni Bourscheid <djonibourscheid00@gmail.com>
2021-04-25 14:29:52 -0300

    estrutura SSG básico na página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,43 @@
-import { Header } from "../components/Header";
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
 
-export default function Home() {
+// type ou interface, tanto faz
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  // ...
+}
+
+type HomeProps = {
+  // Array precisa receber um tipo. Array<string> ['1', 'a', 'b']
+  // Ou podemos usar Episode[]
+  episodes: Episode[];
+}
+
+
+export default function Home(props: HomeProps) {
   return (
     <h1>index</h1>
   )
+}
+
+export const getStaticPorps: GetStaticProps = async () => {
+  // _limit = vai buscar só x registros
+  // _sort = ordenados por..;
+  // _order = crescente ou decrescente
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }

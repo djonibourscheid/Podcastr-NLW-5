f9dd6c7e1e0013d04b8e3d2606dd06ff8edf2d2a
Djoni Bourscheid <djonibourscheid00@gmail.com>
2021-04-28 18:12:16 -0300

    PlayerContext mostrando em tela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import { GetStaticProps } from 'next';
+
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
+
 import Image from 'next/image';
 import Link from 'next/link';
+
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
@@ -29,6 +35,8 @@ type HomeProps = {
 
 
 export default function Home({ lastestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
+
   return (
     <div className={styles.homepage}>
       <section className={styles.lastestEpisodes}>
@@ -58,7 +66,7 @@ export default function Home({ lastestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
@@ -103,7 +111,7 @@ export default function Home({ lastestEpisodes, allEpisodes }: HomeProps) {
                   <td style={{ width: 100 }}>{episode.publishedAt}</td>
                   <td>{episode.durationAsString}</td>
                   <td>
-                    <button type="button">
+                    <button type="button" onClick={() => play(episode)}>
                       <img src="/play-green.svg" alt="Tocar episódio" />
                     </button>
                   </td>

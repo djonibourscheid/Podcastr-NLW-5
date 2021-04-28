e8a7633a814622c94df7c4706551704ba25ba0ae
Djoni Bourscheid <djonibourscheid00@gmail.com>
2021-04-28 18:11:43 -0300

    create PlayerContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,31 @@
 import '../styles/global.scss';
 
+import { useState } from 'react';
+
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+
+  function play(episode) {
+    setCurrentEpisodeIndex(0);
+    setEpisodeList([episode]);
+  }
   return (
-    <div className={styles.appWrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+      <div className={styles.appWrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   )
 
 }
